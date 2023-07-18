fd04f49f79b084d6638beee0c2c556cddf34f07c
Green-EyE <cicero.salomao@gmail.com>
2023-07-18 12:41:01 +0200

    First commit

---
+++ Scripts/ODK+.R
@@ -0,0 +1,41 @@
+#Reshape C_T data-set by Green_Eye
+
+rm(list = ls())
+
+library(readxl)
+library(readr)
+library(dplyr)
+
+
+
+#Read files into R environment
+SIS_Cod <- read_excel("C:/Users/cnhantumbo/Documents/KoboToolbox/ODK/SIS-Cod.xlsx")
+C_T_results1 <- read_csv("C:/Users/cnhantumbo/Documents/KoboToolbox/ODK/Data/C_T_results.csv")
+head(C_T_results1)
+
+#Select columns needed
+C_T_results2 <- select(C_T_results1, 'central_code',	'groupNivelSupervisao:QSupLevel',	'groupNivelSupervisao:supervisor',	'groupGeo:province',	
+                       'groupGeo:district', 'groupGeo:QGeo',	'QSupCode' ,	'groupForm:groupGI:QGIPartner','groupForm:groupDS1:domainScore1',
+                       'groupForm:groupDS2:domainScore2','groupForm:groupDS3:domainScoreG3','groupForm:groupDS4:domainScoreG4','groupForm:groupDS5:domainScoreG5',
+                       'groupForm:groupDS7:domainScore7')
+head(C_T_results2)
+
+#Rename columns
+C_T_results3 <- C_T_results2 %>% rename("central_code"= "central_code" ,	"qSupLevel"= "groupNivelSupervisao:QSupLevel" ,	"supervisor"= "groupNivelSupervisao:supervisor",	"provinceCode"= "groupGeo:province",	
+                       "district"= "groupGeo:district", "qGeo"= "groupGeo:QGeo", "qSupCode"="QSupCode" ,	"qGIPartner"="groupForm:groupGI:QGIPartner","domainScore1"= "groupForm:groupDS1:domainScore1",
+                       "domainScore2"="groupForm:groupDS2:domainScore2", "domainScore3"="groupForm:groupDS3:domainScoreG3", "domainScore4"="groupForm:groupDS4:domainScoreG4" ,"domainScore5"="groupForm:groupDS5:domainScoreG5",
+                       "domainScore7"= "groupForm:groupDS7:domainScore7")
+head(C_T_results3)
+
+# Joint data frames
+C_T_results4 <- inner_join(SIS_Cod, C_T_results3, by=c("SIS-Cod"="qGeo", "Prov-Cod" = "provinceCode", "Dist."= "district"))
+head(C_T_results4)
+
+#Write to disk
+write.csv(C_T_results4, file = "Results/C_T_results.csv")                       
+                       
+                        
+
+                       
+                  
+ 
